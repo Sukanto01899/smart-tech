63a8e605b8c7adf809c658692c1aa5226d054a4d
Sukanto Das <Suknto01899>
2022-10-04 19:24:28 +0600

    show more adding

--- main.js
+++ main.js
@@ -23,21 +23,45 @@ const getData = async (searchText)=>{
 const showSearchResult = data => {
     const phoneResultBox = document.getElementById('phone-result');
     phoneResultBox.textContent = ''
-    for(const phone of data.data){ //loop
-        const mainDiv = document.createElement('div');
-        mainDiv.classList.add('card');
-         mainDiv.innerHTML = `
-         <div class="img">
-                <img src="${phone.image}" alt="">
-                <span>${phone.brand}</span>
-            </div>
-           <div class="card-body">
-            <h4>${phone.phone_name}</h4>
-            <button onclick="fetchSlug('${phone.slug}')">Details</button>
-           </div>`
 
-           phoneResultBox.appendChild(mainDiv);
+    if(data.data.length === 0){
+        console.log('not found')
     }
+    else if(data.data.length > 20){
+        for(const phone of data.data){ //loop
+            const mainDiv = document.createElement('div');
+            mainDiv.classList.add('card');
+             mainDiv.innerHTML = `
+             <div class="img">
+                    <img src="${phone.image}" alt="">
+                    <span>${phone.brand}</span>
+                </div>
+               <div class="card-body">
+                <h4>${phone.phone_name}</h4>
+                <button onclick="fetchSlug('${phone.slug}')">Details</button>
+               </div>`
+    
+               phoneResultBox.appendChild(mainDiv);
+        }
+    }
+    else{
+        for(const phone of data.data){ //loop
+            const mainDiv = document.createElement('div');
+            mainDiv.classList.add('card');
+             mainDiv.innerHTML = `
+             <div class="img">
+                    <img src="${phone.image}" alt="">
+                    <span>${phone.brand}</span>
+                </div>
+               <div class="card-body">
+                <h4>${phone.phone_name}</h4>
+                <button onclick="fetchSlug('${phone.slug}')">Details</button>
+               </div>`
+    
+               phoneResultBox.appendChild(mainDiv);
+        }
+    }
+    
     showSpin('none')
 }
 
@@ -57,8 +81,9 @@ const showDetails = data =>{
                 <img src="${data.data.image}" alt="">
                 <h1>${data.data.name}</h1>
                 <p>${data.data.releaseDate ? data.data.releaseDate : 'Release data not found'}</p>
-            </div>
-            <div class="specification">
+    </div>
+
+    <div class="specification">
                 <div class="mainFeatures">
                     <h3>Main Features</h3>
                     <table>
@@ -89,27 +114,27 @@ const showDetails = data =>{
                     <table>
                         <tr>
                             <td>WLAN</td>
-                            <td>${data.data.others.WLAN}</td>
+                            <td>${data.data.others?.WLAN ? data.data.others.WLAN : 'Not found'}</td>
                         </tr>
                         <tr>
                             <td>Bluetooth</td>
-                            <td>${data.data.others.Bluetooth}</td>
+                            <td>${data.data.others?.Bluetooth ? data.data.others.Bluetooth : 'Not found'}</td>
                         </tr>
                         <tr>
                             <td>GPS</td>
-                            <td>${data.data.others.GPS}</td>
+                            <td>${data.data.others?.GPS ? data.data.others.GPS : 'Not found'}</td>
                         </tr>
                         <tr>
                             <td>NFC</td>
-                            <td>${data.data.others.NFC}</td>
+                            <td>${data.data.others?.NFC ? data.data.others.NFC : 'Not found'}</td>
                         </tr>
                         <tr>
                             <td>Radio</td>
-                            <td>${data.data.others.Radio}</td>
+                            <td>${data.data.others?.Radio ? data.data.others.Radio : 'Not found'}</td>
                         </tr>
                         <tr>
                             <td>USB</td>
-                            <td>${data.data.others.USB}</td>
+                            <td>${data.data.others?.USB ? data.data.others.USB : 'Not found'}</td>
                         </tr>
                     </table>
                 </div>
